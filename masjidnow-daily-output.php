0af58899d244f2c993af14156e62e0db92c6f1f4
yaj786 <yaj786@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-26 04:49:21 +0000

    Tagged version 0.9.1beta
    git-svn-id: https://plugins.svn.wordpress.org/masjidnow/tags/0.9.1beta@687350 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- masjidnow-daily-output.php
+++ masjidnow-daily-output.php
@@ -1,41 +1,106 @@
 <?php
 
-$masjid_name = $salah_timings->masjid_name;
-$fajr = $salah_timings->fajr;
-$dhuhr = $salah_timings->dhuhr;
-$asr = $salah_timings->asr;
-$maghrib = $salah_timings->maghrib;
-$isha = $salah_timings->isha;
+//DEBUG ITEMS TO BE REMOVED!!!!!!!
 
-$date = $salah_timings->month."/".$salah_timings->day."/".$salah_timings->year; 
+$pray_time_high_lats_method = 1; //Midnight
+$pray_time_time_format = 1; //time12
+
+$latitude = 42.649235;
+$longitude = -82.983738;
+
+//DEBUG ITEMS TO BE REMOVED!!!!!!!
+
+
+//wordpress changes this elsewhere, so save it and reset it at the end of the file.
+$original_time_zone_id = date_default_timezone_get();
+
+date_default_timezone_set($time_zone_id);
+$date_time_zone = new DateTimeZone($time_zone_id);
+$date_time_now = new DateTime("now", $date_time_zone);
+
+$masjid_name = $salah_timings->masjid_name;
+
+$pray_time = new PrayTime($pray_time_calc_method);
+$pray_time->setAsrMethod($pray_time_asr_juristic);
+$pray_time->setHighLatsMethod($pray_time_high_lats_method);
+$pray_time->setTimeFormat($pray_time_time_format);
+
+$now_millis = $date_time_now->getTimestamp();
+//gives timezone offset in seconds, we need to convert to hours
+$time_zone = $date_time_now->getOffset()/3600;
+
+
+$calculated_times = $pray_time->getPrayerTimes($now_millis, $latitude, $longitude, $time_zone);
+$adhan_times = array(
+  "fajr" => $calculated_times[0],
+  "sunrise" => $calculated_times[1],
+  "dhuhr" => $calculated_times[2],
+  "asr" => $calculated_times[3],
+  "maghrib" => $calculated_times[4],
+  "sunset" => $calculated_times[5],
+  "isha" => $calculated_times[6],
+);
+
+$iqamah_times = array(
+  "fajr" => $salah_timings->fajr,
+  "dhuhr" => $salah_timings->dhuhr,
+  "asr" => $salah_timings->asr,
+  "maghrib" => $salah_timings->maghrib,
+  "isha" => $salah_timings->isha,
+);
+
+$iqamah_date_str = $salah_timings->month."/".$salah_timings->day."/".$salah_timings->year; 
 ?>
 
 <div class="masjidnow-container <?php echo($theme);?>">
 
  <div class='masjidnow-masjid-name'>Iqamah Timings for <br/><?php echo($masjid_name); ?></div>
  <table class='masjidnow-salah-timings-table'>
+  
+  <tr>
+    <th></th>
+    <th>Adhan</th>
+    <th>Iqamah</th>
+  </tr>
+   
   <tr class='masjid-salah-row'>
     <td class='masjidnow-salah-name masjidnow-fajr'>Fajr</td>
-    <td class='masjidnow-salah-time masjidnow-fajr'><?php echo($fajr); ?></td>
+    <td class='masjidnow-salah-time-adhan masjidnow-fajr'><?php echo($adhan_times["fajr"]); ?></td>
+    <td class='masjidnow-salah-time-iqamah masjidnow-fajr'><?php echo($iqamah_times["fajr"]); ?></td>
   </tr>
   <tr class='masjid-salah-row masjid-salah-row-alt'>
+    <td class='masjidnow-salah-name masjidnow-dhuhr'>Sunrise</td>
+    <td class='masjidnow-salah-time-adhan masjidnow-sunrise'><?php echo($adhan_times["sunrise"]); ?></td>
+    <td><!-- Empty but necessary --></td>
+  </tr>
+  <tr class='masjid-salah-row'>
     <td class='masjidnow-salah-name masjidnow-dhuhr'>Dhuhr</td>
-    <td class='masjidnow-salah-time masjidnow-dhuhr'><?php echo($dhuhr); ?></td>
-  </tr>
-  <tr class='masjid-salah-row'>
-    <td class='masjidnow-salah-name masjidnow-asr'>Asr</td>
-    <td class='masjidnow-salah-time masjidnow-asr'><?php echo($asr); ?></td>
+    <td class='masjidnow-salah-time-adhan masjidnow-dhuhr'><?php echo($adhan_times["dhuhr"]); ?></td>
+    <td class='masjidnow-salah-time-iqamah masjidnow-dhuhr'><?php echo($iqamah_times["dhuhr"]); ?></td>
   </tr>
   <tr class='masjid-salah-row masjid-salah-row-alt'>
-    <td class='masjidnow-salah-name masjidnow-maghrib'>Maghrib</td>
-    <td class='masjidnow-salah-time masjidnow-maghrib'><?php echo($maghrib); ?></td>
+    <td class='masjidnow-salah-name masjidnow-asr'>Asr</td>
+    <td class='masjidnow-salah-time-adhan masjidnow-asr'><?php echo($adhan_times["asr"]); ?></td>
+    <td class='masjidnow-salah-time-iqamah masjidnow-asr'><?php echo($iqamah_times["asr"]); ?></td>
   </tr>
   <tr class='masjid-salah-row'>
+    <td class='masjidnow-salah-name masjidnow-maghrib'>Maghrib</td>
+    <td class='masjidnow-salah-time-adhan masjidnow-maghrib'><?php echo($adhan_times["maghrib"]); ?></td>
+    <td class='masjidnow-salah-time-iqamah masjidnow-maghrib'><?php echo($iqamah_times["maghrib"]); ?></td>
+  </tr>
+  <tr class='masjid-salah-row masjid-salah-row-alt'>
     <td class='masjidnow-salah-name masjidnow-isha'>Isha</td>
-    <td class='masjidnow-salah-time masjidnow-isha'><?php echo($isha); ?></td>
+    <td class='masjidnow-salah-time-adhan masjidnow-isha'><?php echo($adhan_times["isha"]); ?></td>
+    <td class='masjidnow-salah-time-iqamah masjidnow-isha'><?php echo($iqamah_times["isha"]); ?></td>
   </tr>
  </table>
  
- <div class='masjidnow-date'>From <?php echo($date) ?></div>
+ <div class='masjidnow-date'>
+   Iqamah timings from <?php echo($iqamah_date_str) ?> <br/>
+   Salah timings from <?php echo($date_time_now->format("D j M,  Y")) ?>
+ </div>
  
 </div>
+
+
+<?php date_default_timezone_set($original_time_zone_id); ?>
--- readme.txt
+++ readme.txt
@@ -4,15 +4,16 @@ Donate link:
 Tags: masjidnow, iqamah, salah, islam, masjid, mosque, salat, prayer
 Requires at least: 3.5.1
 Tested up to: 3.5.1
-Stable tag: "trunk"
+Stable tag: 0.9.1beta
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there.
+Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there, as well as calculated prayer times for an area.
 
 == Description ==
 
-Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there.
+Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there. <br/>
+The timings are displayed along-side calculated timings which can be adjusted from the plugin's settings.
 
 == Installation ==
 
@@ -21,6 +22,7 @@ This section describes how to install the plugin and get it working.
 1. Upload the `masjidnow` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Place the `MasjidNow Daily Iqamah Timings` widget in the correct area through the `Appearance` > `Widgets` menu in WordPress.
+4. Change the widget's settings to your preference, then press save.
 
 == Frequently Asked Questions ==
 
@@ -29,6 +31,12 @@ This section describes how to install the plugin and get it working.
 Email me through the contact form at masjidnow.com
 
 == Changelog ==
+
+= 0.9.1 =
+* Added ability to show calculated prayer timings.
+
+= 0.9 =
+* No difference, simply a version bump.
 
 = 0.1 =
 * Right now, the widget only displays the latest timings.
