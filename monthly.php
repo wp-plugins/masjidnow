940f9cc27c7ccd06e93126486dea8c096d5fe56a
yaj786 <yaj786@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-07 17:59:33 +0000

    Fixed iqamah timings date bug, and tagged v0.9.9
    git-svn-id: https://plugins.svn.wordpress.org/masjidnow/tags/0.9.9@709372 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ monthly.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MasjidNowMonthly;
+
+function getOutput($attrs)
+{
+  extract( shortcode_atts( array(
+		'masjid_id' => null,
+	), $attrs ) );
+  
+  $date_time_now = new \DateTime("now", new \DateTimeZone(get_option('timezone_string')));
+  
+  if(!empty($masjid_id))
+  {
+    $timings = get_timings($masjid_id, $date_time_now, null, null);
+    ob_start();
+    include("monthly-output.php");
+    $output_string = ob_get_contents();
+    ob_end_clean();
+    return $output_string;
+  }
+  else
+  {
+    return "ERROR! Please set the masjid_id by adding it to the end of the shortcode. (ie. [masjidnow_monthly masjid_id=1234])";
+  }
+  
+}
+
+function get_timings($masjid_id, $date_time_now)
+{
+  $api_helper = new \MasjidNow\APIHelper($masjid_id, $date_time_now, null, null);
+  $timings = $api_helper->get_monthly_timings();
+  return $timings["iqamah_timings"];
+}
+
+?>
