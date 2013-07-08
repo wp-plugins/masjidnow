3b1acfdce6a11295cd3446a07ff052eca3be53f1
yaj786 <yaj786@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-08 22:06:49 +0000

    Fixing widget not being able to go backwards sometimes. Tagging version 0.9.11.
    git-svn-id: https://plugins.svn.wordpress.org/masjidnow/tags/0.9.11@737982 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-praytime-helper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MasjidNow;
+
+require_once("libs/PrayTime.php");
+
+class PrayTimeHelper{
+  
+  static function get_pray_time($settings)
+  {
+    $pray_time = new \PrayTime($settings["calc_method"]);
+    $pray_time->setAsrMethod($settings["asr_juristic"]);
+    $pray_time->setHighLatsMethod($settings["high_lats"]);
+    $pray_time->setTimeFormat($settings["time_format"]);
+    return $pray_time;
+  }
+  
+  static function get_pray_time_settings_from_widget($instance)
+  {
+    $pray_time_calc_method = empty($instance['pray-time-calc-method']) ? 2 : $instance['pray-time-calc-method']; 
+    $pray_time_asr_juristic = empty($instance['pray-time-asr-juristic']) ? 2 : $instance['pray-time-asr-juristic']; 
+    $pray_time_high_lats_method = 1; //Midnight
+    $pray_time_time_format = 1; //time12
+    $settings = array(
+      "asr_juristic" => $pray_time_asr_juristic,
+      "calc_method" => $pray_time_calc_method,
+      "high_lats" => $pray_time_high_lats_method,
+      "time_format" => $pray_time_time_format,
+    );
+    return $settings;
+  }
+  
+  static function get_location($instance)
+  {
+    $latitude = empty($instance['latitude']) ? 2 : $instance['latitude']; 
+    $longitude = empty($instance['longitude']) ? 2 : $instance['longitude'];
+    $time_zone_id = empty($instance['time-zone-id']) ? "America/New_York" : $instance['time-zone-id'];
+    return array(
+      "latitude" => $latitude,
+      "longitude" => $longitude,
+      "time_zone_id" => $time_zone_id,
+    ); 
+  }
+  
+}
+
+?>
