0fea6c72604506cb7f82c0393418da37cbacdf53
yaj786 <yaj786@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-12 21:08:35 +0000

    Tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/masjidnow/tags/1.0.2@740018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- masjidnow-daily-output.php
+++ masjidnow-daily-output.php
@@ -1,171 +1,167 @@
 <?php
 
-
-$pray_time_high_lats_method = 1; //Midnight
-$pray_time_time_format = 1; //time12
-
-$date_time_now = new DateTime("now", $date_time_zone);
-  
-$adhan_times = array(
-  "fajr" => "",
-  "sunrise" => "",
-  "dhuhr" => "",
-  "asr" => "",
-  "maghrib" => "",
-  "sunset" => "",
-  "isha" => "",
-);
-
-$iqamah_times = array(
-  "fajr" => "",
-  "dhuhr" => "",
-  "asr" => "",
-  "maghrib" => "",
-  "isha" => "",
-);
-
-$local_calculated = false;
 $iqamah_date_str = "";
 
-$api_iqamah_timings_exist = $response && isset($response->masjid) && isset($response->masjid->salah_timings);
+$masjid_exists = $api_helper->does_masjid_exist();
 
-$response_valid = $response != NULL;
-$masjid_exists = $response != NULL;
 
 if($masjid_exists)
 {
-  $masjid_name = $response->masjid->name;
+  $location_name = $response["raw"]->masjid->name;
 }
 else
 {
   if(isset($masjid_id))
   {
-    $masjid_name = "Masjid with id $masjid_id Not Found";
+    $location_name = "Masjid with id $masjid_id Not Found";
   }
   else
   {
-    $masjid_name = "$latitude, $longitude";
+    $location_name = $location["latitude"].", ".$location["longitude"];
   }
 }
 
-if(!$api_iqamah_timings_exist)
-{
-  $local_calculated = true;
-  
-  $now_millis = $date_time_now->getTimestamp();
-  //gives timezone offset in seconds, we need to convert to hours
-  $time_zone = $date_time_now->getOffset()/3600;
-  $pray_time = new PrayTime($pray_time_calc_method);
-  $pray_time->setAsrMethod($pray_time_asr_juristic);
-  $pray_time->setHighLatsMethod($pray_time_high_lats_method);
-  $pray_time->setTimeFormat($pray_time_time_format);
-  $calculated_times = $pray_time->getPrayerTimes($now_millis, $latitude, $longitude, $time_zone);
-  $adhan_times = array(
-    "fajr" => $calculated_times[0],
-    "sunrise" => $calculated_times[1],
-    "dhuhr" => $calculated_times[2],
-    "asr" => $calculated_times[3],
-    "maghrib" => $calculated_times[4],
-    "sunset" => $calculated_times[5],
-    "isha" => $calculated_times[6],
-  );
-}
-else
-{
-  $local_calculated = false;
-  $masjid = $response->masjid;
-  $masjid_name = $masjid->name;
-  if(isset($masjid->salah_timings))
-  {
-    $salah_timings = $masjid->salah_timings;
-    $salah_timing = $this->get_closest_timing($date_time_now, $salah_timings);
-    $adhan_times = array(
-      "fajr" => $salah_timing->fajr_adhan,
-      "sunrise" => $salah_timing->sunrise_adhan,
-      "dhuhr" => $salah_timing->dhuhr_adhan,
-      "asr" =>$salah_timing->asr_adhan,
-      "maghrib" => $salah_timing->maghrib_adhan,
-      "isha" => $salah_timing->isha_adhan
-    );
-    
-    $iqamah_times = array(
-      "fajr" => $salah_timing->fajr,
-      "dhuhr" => $salah_timing->dhuhr,
-      "asr" => $salah_timing->asr,
-      "maghrib" => $salah_timing->maghrib,
-      "isha" => $salah_timing->isha,
-    );
-    
-    $iqamah_date_time = new DateTime($salah_timing->year."-".$salah_timing->month."-".$salah_timing->day);
-    $iqamah_date_str = "Iqamah timings for ".$iqamah_date_time->format("D M j, Y");
-  }  
+$iqamah_date_str = "Iqamah timings for ".$api_helper->get_iqamah_date_str("D M j, Y");
 
-}
+$show_iqamah = $this->should_show_iqamah($api_helper);
+$show_adhan = $this->should_show_adhan($instance);
+$row_count = 0;
 
+$is_timings_old = false;
+$iqamah_date_time = $api_helper->get_iqamah_date();
+if($iqamah_date_time)
+{
+  $diff = $iqamah_date_time->diff(new DateTime());
+  if ($diff->format('%a') > '0') {
+      $is_timings_old = true;
+  } 
+}
 
 //$iqamah_date_str = $salah_timings->month."/".$salah_timings->day."/".$salah_timings->year; 
 ?>
+<?php 
+  if(!empty($title))
+  {
+    echo($before_title . $title . $after_title);
+  }
+?>     
 
+  <div>
+  </div>
+
+<?php
+  if($response["raw"] && $response["raw"]->masjid)
+  {
+  ?>
+      <script type="text/javascript">
+        WPMasjidNowWidget.saveTimings(<?php echo(json_encode($response["raw"]->masjid)) ?>);
+      </script>
+  <?php
+  }
+?>
+ 
 <div class="masjidnow-container <?php echo($theme);?>">
 
- <div class='masjidnow-masjid-name'><?php echo($masjid_name); ?></div>
+  <?php if($show_iqamah) { ?>
+  <div class="masjidnow-prev-day"></div>
+  <div class="masjidnow-next-day"></div>
+  <?php } ?>
+ <div class='masjidnow-date'>
+  <?php echo($date_time_now->format("D M j, Y"))?>
+ </div>
+
  <table class='masjidnow-salah-timings-table'>
   
   <tr>
     <th></th>
-    <th>Adhan</th>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <th>Adhan</th>
+    <?php } ?>
+    <?php if($show_iqamah) { ?>
       <th>Iqamah</th>
     <?php } ?>
   </tr>
    
-  <tr class='masjid-salah-row'>
+  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
     <td class='masjidnow-salah-name masjidnow-fajr'>Fajr</td>
-    <td class='masjidnow-salah-time-adhan masjidnow-fajr'><?php echo($adhan_times["fajr"]); ?></td>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <td class='masjidnow-salah-time-adhan masjidnow-fajr'><?php echo($adhan_times["fajr"]); ?></td>
+    <?php } ?>    
+    <?php if($show_iqamah) { ?>
       <td class='masjidnow-salah-time-iqamah masjidnow-fajr'><?php echo($iqamah_times["fajr"]); ?></td>
     <?php } ?>
   </tr>
-  <tr class='masjid-salah-row masjid-salah-row-alt'>
+  <?php if($show_adhan) { ?>
+  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
     <td class='masjidnow-salah-name masjidnow-dhuhr'>Sunrise</td>
-    <td class='masjidnow-salah-time-adhan masjidnow-sunrise'><?php echo($adhan_times["sunrise"]); ?></td>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <td class='masjidnow-salah-time-adhan masjidnow-sunrise'><?php echo($adhan_times["sunrise"]); ?></td>
+    <?php } ?>
+    <?php if($show_iqamah) { ?>
       <td><!-- Empty but necessary --></td>
     <?php } ?>
   </tr>
-  <tr class='masjid-salah-row'>
+  <?php } ?>
+  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
     <td class='masjidnow-salah-name masjidnow-dhuhr'>Dhuhr</td>
-    <td class='masjidnow-salah-time-adhan masjidnow-dhuhr'><?php echo($adhan_times["dhuhr"]); ?></td>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <td class='masjidnow-salah-time-adhan masjidnow-dhuhr'><?php echo($adhan_times["dhuhr"]); ?></td>
+    <?php } ?>
+    <?php if($show_iqamah) { ?>
       <td class='masjidnow-salah-time-iqamah masjidnow-dhuhr'><?php echo($iqamah_times["dhuhr"]); ?></td>
     <?php } ?>
   </tr>
-  <tr class='masjid-salah-row masjid-salah-row-alt'>
+  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
     <td class='masjidnow-salah-name masjidnow-asr'>Asr</td>
-    <td class='masjidnow-salah-time-adhan masjidnow-asr'><?php echo($adhan_times["asr"]); ?></td>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <td class='masjidnow-salah-time-adhan masjidnow-asr'><?php echo($adhan_times["asr"]); ?></td>
+    <?php } ?>
+    <?php if($show_iqamah) { ?>
       <td class='masjidnow-salah-time-iqamah masjidnow-asr'><?php echo($iqamah_times["asr"]); ?></td>
     <?php } ?>
   </tr>
-  <tr class='masjid-salah-row'>
+  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
     <td class='masjidnow-salah-name masjidnow-maghrib'>Maghrib</td>
-    <td class='masjidnow-salah-time-adhan masjidnow-maghrib'><?php echo($adhan_times["maghrib"]); ?></td>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <td class='masjidnow-salah-time-adhan masjidnow-maghrib'><?php echo($adhan_times["maghrib"]); ?></td>
+    <?php } ?>
+    <?php if($show_iqamah) { ?>
       <td class='masjidnow-salah-time-iqamah masjidnow-maghrib'><?php echo($iqamah_times["maghrib"]); ?></td>
     <?php } ?>
   </tr>
-  <tr class='masjid-salah-row masjid-salah-row-alt'>
+  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
     <td class='masjidnow-salah-name masjidnow-isha'>Isha</td>
-    <td class='masjidnow-salah-time-adhan masjidnow-isha'><?php echo($adhan_times["isha"]); ?></td>
-    <?php if($api_iqamah_timings_exist) { ?>
+    <?php if($show_adhan) { ?>
+      <td class='masjidnow-salah-time-adhan masjidnow-isha'><?php echo($adhan_times["isha"]); ?></td>
+    <?php } ?>
+    <?php if($show_iqamah) { ?>
       <td class='masjidnow-salah-time-iqamah masjidnow-isha'><?php echo($iqamah_times["isha"]); ?></td>
     <?php } ?>
   </tr>
  </table>
  
- <div class='masjidnow-date'>
-   <?php echo($iqamah_date_str) ?> <br/>
-   Salah timings for <?php echo($date_time_now->format("D M j, Y")) ?>
+ <?php if($show_iqamah) { ?>
+   <div class='masjidnow-iqamah-date'>
+     <?php if($is_timings_old) { ?>
+      <?php echo($iqamah_date_str) ?> <br/>
+     <? } ?>
+   </div>
+ <?php } ?>
+ 
+ <div class='masjidnow-masjid-name'>
+  <?php echo($location_name); ?>
  </div>
  
+ <div class='masjidnow-daily-footer'>
+  <span class='masjidnow-alt-instructions'>Get these timings on:</span>
+  <div class='masjidnow-alt-icons'>
+    <div class='masjidnow-alt-icon'>
+      <a href='http://masjidnow.com/masjids/<?php echo($masjid_id)?>#mobile_instructions'><img src='<?php echo(plugins_url( "img\ic_android.png", __FILE__ )); ?>' height=30></a>
+    </div>
+    <div class='masjidnow-alt-icon'>
+      <a href='http://masjidnow.com/masjids/<?php echo($masjid_id)?>#mobile_instructions'><img src='<?php echo(plugins_url( "img\ic_ios.png", __FILE__ )); ?>' height=30></a>
+    </div>
+  </div>
+ </div>
+
 </div>
--- readme.txt
+++ readme.txt
@@ -4,16 +4,18 @@ Donate link:
 Tags: masjidnow, iqamah, salah, islam, masjid, mosque, salat, prayer
 Requires at least: 3.5.1
 Tested up to: 3.5.1
-Stable tag: 0.9.3
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there, as well as calculated prayer times for an area.
+Provides the ability to display daily and monthly adhan and iqamah timings from MasjidNow.com. Local adhan times calculation available if necessary.
 
 == Description ==
 
-Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there. <br/>
-The timings are displayed along-side calculated timings which can be adjusted from the plugin's settings.
+Provides a widget for displaying daily iqamah timings from MasjidNow.com for any mosque that has uploaded it's timings there.
+The timings are displayed along-side adhan timings which can be adjusted from the plugin's settings if necessary.
+
+Also provides a shortcode for displaying the monthly iqamah timings table.
 
 == Installation ==
 
@@ -21,8 +23,16 @@ This section describes how to install the plugin and get it working.
 
 1. Upload the `masjidnow` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Place the `MasjidNow Daily Iqamah Timings` widget in the correct area through the `Appearance` > `Widgets` menu in WordPress.
-4. Change the widget's settings to your preference, then press save.
+
+To use the daily timings sidebar widget:
+
+1. Place the `MasjidNow Daily Iqamah Timings` widget in the correct area through the `Appearance` > `Widgets` menu in WordPress.
+2. Change the widget's settings to your preference, then press save.
+
+To show the monthly timings table anywhere on your site:
+
+1. Place the shortcode [masjidnow_monthly masjid_id=XXXX], replacing XXXX with your mosque's MasjidNow.com id. This can be found at the end of the MasjidNow.com url for your mosque.
+For example, http://masjidnow.com/masjids/5312 has a masjid_id of 5312. Simple!
 
 == Frequently Asked Questions ==
 
@@ -31,6 +41,48 @@ This section describes how to install the plugin and get it working.
 Email me through the contact form at masjidnow.com
 
 == Changelog ==
+
+= 1.0.2 =
+* Changed style of widget to properly accomodate small sidebars.
+
+= 1.0.1 =
+* Switched to using wp_remote_get instead of file_get_contents which may cause issues on some WordPress installations.
+
+= 1.0.0 =
+* Added mobile app download links.
+* Changed iqamah timings date to only show if timings were old.
+* Added support for PHP 5 (previous support was PHP 5.3)
+
+= 0.9.11 =
+* Fixed bug where widget couldn't go in the past if a month was incomplete.
+
+= 0.9.10 =
+* Included updated file from fix in 0.9.9
+
+= 0.9.9 =
+* Fixed bug where iqamah timings date would show "No timings available" regardless of availability.
+
+= 0.9.8 =
+* Added ability to display monthly timings table.
+
+= 0.9.7 =
+* Added ability to only display iqamah timings, instead of both iqamah and adhan timings.
+* Fixed bug where initial iqamah and adhan timings were showing the timings from the last day of the month instead of the correct day's.
+
+= 0.9.6 =
+* Added ability to change date.
+
+= 0.9.5 =
+* Removed border and changed some stylings.
+* Fixed bugs with old adhan timings being shown.
+
+= 0.9.4 =
+* Removed width requirement on container. Widget can now fit into smaller areas.
+* Added ability to set the header.
+
+= 0.9.3 =
+* Upgraded to using masjidnow.com API V2. Adhan timings are now gotten from the MasjidNow server instead of through calculations, unless necessary.
+* Widget can be used without having a masjid connected to it. (Will get around to fixing how it looks when there is no masjid connected.)
 
 = 0.9.2 =
 * Fixed incorrect calculated salah times due to wrong lat/lon being used.
