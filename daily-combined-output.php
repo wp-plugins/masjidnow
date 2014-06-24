b28ac3ee11d96ba07f569656e39f6e0191926a92
yaj786 <yaj786@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-24 21:42:25 +0000

    Added hijri date output.
    git-svn-id: https://plugins.svn.wordpress.org/masjidnow/tags/1.3.2@938073 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daily-combined-output.php
+++ daily-combined-output.php
@@ -1,90 +1,27 @@
 <?php
-
-$iqamah_date_str = "";
-
-$masjid_exists = $api_helper->does_masjid_exist();
-
-
-if($masjid_exists)
-{
-  $location_name = $response["raw"]->masjid->name;
-}
-else
-{
-  if(isset($masjid_id))
-  {
-    $location_name = "Masjid with id $masjid_id Not Found";
-  }
-  else
-  {
-    $location_name = $location["latitude"].", ".$location["longitude"];
-  }
-}
-
-$iqamah_date_str = "Iqamah timings for ".$api_helper->get_iqamah_date_str("D M j, Y");
-$hijri_date_str = "";
-if(isset($iqamah_times["hijri_date"]) && $iqamah_times["hijri_date"] != "")
-{
-  $hijri_date_str = $iqamah_times["hijri_date"];
-}
-
-
-$show_iqamah = $this->should_show_iqamah($api_helper);
-$show_adhan = $this->should_show_adhan($instance);
-$show_monthly_info = $this->should_show_monthly_info($instance);
 $row_count = 0;
-
-$is_timings_old = false;
-$iqamah_date_time = $api_helper->get_iqamah_date();
-if($iqamah_date_time)
-{
-  $today_date_time = new DateTime();
-  $ms_diff = $today_date_time->format('U') - $iqamah_date_time->format('U');
-  $days_diff = $ms_diff / (60*60*24);
-  if ($days_diff >= 1) {
-      $is_timings_old = true;
-  } 
-  // ********* THIS WAY REQUIRES PHP 5.3+ ****************
-  //$diff = $iqamah_date_time->diff(new DateTime());
-  //if ($diff->format('%a') > '0') {
-  //    $is_timings_old = true;
-  //} 
-}
-
-//$iqamah_date_str = $salah_timings->month."/".$salah_timings->day."/".$salah_timings->year; 
 ?>
 <?php 
-  if(!empty($title))
-  {
-    echo($before_title . $title . $after_title);
-  }
+  //if(!empty($title))
+  //{
+  //  echo($before_title . $title . $after_title);
+  //}
 ?>     
 
-  <div>
-  </div>
 
-<?php
-  if($response["raw"] && $response["raw"]->masjid)
-  {
-  ?>
-      <script type="text/javascript">
-        WPMasjidNowWidget.saveTimings(<?php echo(json_encode($response["raw"]->masjid)) ?>);
-      </script>
-  <?php
-  }
-?>
- 
-<div class="masjidnow-container <?php echo($theme);?>">
+<script type="text/javascript">
+  WPMasjidNowWidget.saveTimings(<?php echo(json_encode($response["raw"]->masjid)) ?>);
+</script>
+
+<div class="masjidnow-container <?php echo($theme); ?>">
 
   <?php if($show_iqamah) { ?>
   <div class="masjidnow-prev-day"></div>
   <div class="masjidnow-next-day"></div>
   <?php } ?>
  <div class='masjidnow-date'>
-  <?php echo($date_time_now->format("D M j, Y"))?> <br/>
-  <?php echo($hijri_date_str) ?>
+  <?php echo($date_time_now->format("D M j, Y"))?>
  </div>
- 
 
  <table class='masjidnow-salah-timings-table'>
   
@@ -98,7 +35,7 @@ if($iqamah_date_time)
     <?php } ?>
   </tr>
    
-  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
+  <tr class='masjidnow-salah-row'>
     <td class='masjidnow-salah-name masjidnow-fajr'>Fajr</td>
     <?php if($show_adhan) { ?>
       <td class='masjidnow-salah-time-adhan masjidnow-fajr'><?php echo($adhan_times["fajr"]); ?></td>
@@ -108,7 +45,7 @@ if($iqamah_date_time)
     <?php } ?>
   </tr>
   <?php if($show_adhan) { ?>
-  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
+  <tr class='masjidnow-salah-row'>
     <td class='masjidnow-salah-name masjidnow-dhuhr'>Sunrise</td>
     <?php if($show_adhan) { ?>
       <td class='masjidnow-salah-time-adhan masjidnow-sunrise'><?php echo($adhan_times["sunrise"]); ?></td>
@@ -118,7 +55,7 @@ if($iqamah_date_time)
     <?php } ?>
   </tr>
   <?php } ?>
-  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
+  <tr class='masjidnow-salah-row'>
     <td class='masjidnow-salah-name masjidnow-dhuhr'>Dhuhr</td>
     <?php if($show_adhan) { ?>
       <td class='masjidnow-salah-time-adhan masjidnow-dhuhr'><?php echo($adhan_times["dhuhr"]); ?></td>
@@ -127,7 +64,7 @@ if($iqamah_date_time)
       <td class='masjidnow-salah-time-iqamah masjidnow-dhuhr'><?php echo($iqamah_times["dhuhr"]); ?></td>
     <?php } ?>
   </tr>
-  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
+  <tr class='masjidnow-salah-row'>
     <td class='masjidnow-salah-name masjidnow-asr'>Asr</td>
     <?php if($show_adhan) { ?>
       <td class='masjidnow-salah-time-adhan masjidnow-asr'><?php echo($adhan_times["asr"]); ?></td>
@@ -136,7 +73,7 @@ if($iqamah_date_time)
       <td class='masjidnow-salah-time-iqamah masjidnow-asr'><?php echo($iqamah_times["asr"]); ?></td>
     <?php } ?>
   </tr>
-  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
+  <tr class='masjidnow-salah-row'>
     <td class='masjidnow-salah-name masjidnow-maghrib'>Maghrib</td>
     <?php if($show_adhan) { ?>
       <td class='masjidnow-salah-time-adhan masjidnow-maghrib'><?php echo($adhan_times["maghrib"]); ?></td>
@@ -145,7 +82,7 @@ if($iqamah_date_time)
       <td class='masjidnow-salah-time-iqamah masjidnow-maghrib'><?php echo($iqamah_times["maghrib"]); ?></td>
     <?php } ?>
   </tr>
-  <?php echo($this->get_salah_row_start_tag($row_count++)) ?>
+  <tr class='masjidnow-salah-row'>
     <td class='masjidnow-salah-name masjidnow-isha'>Isha</td>
     <?php if($show_adhan) { ?>
       <td class='masjidnow-salah-time-adhan masjidnow-isha'><?php echo($adhan_times["isha"]); ?></td>
@@ -180,10 +117,10 @@ if($iqamah_date_time)
   <span class='masjidnow-alt-instructions'>Get these timings on:</span>
   <div class='masjidnow-alt-icons'>
     <div class='masjidnow-alt-icon'>
-      <a href='http://www.masjidnow.com<?php echo($masjid_url)?>#mobile_instructions'><img src='<?php echo(plugins_url( "img\ic_android.png", __FILE__ )); ?>' height=30></a>
+      <a href='<?php echo($masjid_url)?>#mobile_instructions' title="Get iqamah timings for <?php echo($location_name)?> on Android "><img src='<?php echo(plugins_url( "img\ic_android.png", __FILE__ )); ?>' height=30></a>
     </div>
     <div class='masjidnow-alt-icon'>
-      <a href='http://www.masjidnow.com<?php echo($masjid_url)?>#mobile_instructions'><img src='<?php echo(plugins_url( "img\ic_ios.png", __FILE__ )); ?>' height=30></a>
+      <a href='<?php echo($masjid_url)?>#mobile_instructions' title="Get iqamah timings for <?php echo($location_name)?> on iOS"><img src='<?php echo(plugins_url( "img\ic_ios.png", __FILE__ )); ?>' height=30></a>
     </div>
   </div>
  </div>
